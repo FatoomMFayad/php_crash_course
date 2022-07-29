da2293dea6ee73d688f4cbeaadeda21e2f6cec63
Fatoom Fayad <fatoomfayyad@gmail.com>
2022-07-29 12:03:10 +0300

    String Concatation

--- strings.php
+++ strings.php
@@ -5,4 +5,7 @@ $str = 'Hello, I am '.$name. ' From PHP';
 $str2 = "Hello, I am $name  From PHP";
 echo $str.'<br>';
 echo $str2.'<br>';
+/****************************/
+//String Concatation
+echo 'Hi '.'World'.'<br>';
 ?>
