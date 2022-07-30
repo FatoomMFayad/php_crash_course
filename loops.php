b6d8f524b11e213e561e6dbe5c751032677236f3
Fatoom Fayad <fatoomfayyad@gmail.com>
2022-07-30 20:19:26 +0300

    iterate over associative array

--- loops.php
+++ loops.php
@@ -22,4 +22,20 @@ $vegetables = ["Tomato", "Onion", "Potato", "Pepper"];
 foreach($vegetables as $i=>$v) {
     echo $i.'- '. $v.'<br>';
 }
+/******************** */
+//Iterate over associative array
+$person = [
+    'name' => 'Ola',
+    'suname' => 'Ali',
+    'Age' => 18,
+    'Skills'=> ['SQL', 'PHP','JS'],
+];
+foreach($person as $key => $p) {    
+    if(is_array($p)){
+     echo $key.':'.implode(", ", $p).'<br>';   
+    }else{
+        echo $key.':' . $p.'<br>';  
+    }
+    
+}
 ?>
