e41fe36864baf42c8ea566561aace2cc685c5a1e
Fatoom Fayad <fatoomfayyad@gmail.com>
2022-07-30 10:39:26 +0300

    set and get array elements

--- arrays.php
+++ arrays.php
@@ -1,5 +1,18 @@
 <?php
 //Create array
 $fruits = ["Apple", "Melon", "Kiwi"];
-
+/******************************/
+//Print the whole array
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
+/******************************/
+//Get element by index
+echo $fruits[1].'<br>';
+/******************************/
+//set element by index
+$fruits[3] = 'Peach';
+echo '<pre>';
+var_dump($fruits);
+echo '</pre>';
 ?>
