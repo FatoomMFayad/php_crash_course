912afc1f584a925c843ba9d08ac6a99324b2f3c2
Fatoom Fayad <fatoomfayyad@gmail.com>
2022-07-30 20:05:57 +0300

    loops

---
+++ loops.php
@@ -0,0 +1,25 @@
+<?php
+$age = 10;
+//while
+// while ($age > 0) {
+//     echo $age.'<br>';
+//     $age--;
+// }
+/**************************/
+//do-while
+// do {
+//     echo $age.'<br>';
+//     $age--;
+// }while($age > 0);
+/**************************** */
+//for loop
+for ($i = 0 ; $i < $age; $i++) {
+    echo $i.'<br>';
+}
+/******************* */
+//foreach
+$vegetables = ["Tomato", "Onion", "Potato", "Pepper"];
+foreach($vegetables as $v) {
+    echo $v.'<br>';
+}
+?>
